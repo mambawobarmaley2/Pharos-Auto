39ff5c471c075a861040f3a4ee5929f7649222fe
cryptosofter199 <lajimihafex@hotmail.com>
2025-09-05 21:57:20 +0000

    install libraries in one click
    created a install libraries.bat file for those who don't know how to do it manually. Simply click it and it will install everything you will need.

---
+++ install libraries.bat
@@ -0,0 +1,13 @@
+@echo off
+pip install web3==7.11.1
+pip install eth-account==0.13.7
+pip install eth-utils==5.3.0
+pip install eth-abi==5.2.0
+pip install aiohttp==3.11.10
+pip install aiohttp-socks==0.9.1
+pip install websockets==13.0
+pip install fake-useragent==1.5.1
+pip install colorama==0.4.6
+pip install pytz==2024.1
+pip install requests==2.32.3
+pause
